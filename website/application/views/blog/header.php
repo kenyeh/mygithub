86d73f878dbed682add8ed1740089c6acbc53c73
kenyeh <ken90209@gmail.com>
2015-04-02 05:41:58 +0000

    test123

--- website/application/views/blog/header.php
+++ website/application/views/blog/header.php
@@ -216,7 +216,7 @@
     <script src="<?= base_url()?>resource/js/jquery.min.js"></script>
     <script src="<?= base_url()?>resource/js/bootstrap.min.js"></script>
     
-	<!--123-->
+	<!--123dddd-->
 	<script src="<?= base_url()?>resource/js/MY_main.js"></script>
 	
 	
